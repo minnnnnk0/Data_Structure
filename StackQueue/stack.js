0d590aae236cac40c748bf657d0dbe8b25aaf23c
minnnnnk0 <kjjpyckjs@naver.com>
2024-08-04 15:55:06 +0900

    Stack

---
+++ StackQueue/stack.js
@@ -0,0 +1,32 @@
+class Stack {
+  arr = [];
+
+  push(value) {
+    // push의 return 값은 length
+    return this.arr.push(value);
+  }
+
+  pop() {
+    return this.arr.pop();
+  }
+
+  top() {
+    // return this.arr[this.arr.length - 1];  예전 방법
+    return this.arr.at(-1);
+  }
+
+  //get을 쓰면 메소드가 아닌 getter라고 부른다. (배열의 length)
+  get length() {
+    return this.arr.length;
+  }
+}
+
+const stack = new Stack();
+stack.push(1);
+stack.push(3);
+stack.push(5);
+stack.push(2);
+stack.push(4); // 5
+console.log(stack.length); // 5
+stack.pop(); // 4
+console.log(stack.top); // 2
